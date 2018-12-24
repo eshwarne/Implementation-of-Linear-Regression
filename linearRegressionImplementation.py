a819b88eeaf7c64f2ac80f63205fa79507a96495
eshwarne <northeasteditsofficial@gmail.com>
2018-12-24 15:50:43 +0530

    best fit line slope

--- linearRegressionImplementation.py
+++ linearRegressionImplementation.py
@@ -2,8 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt;
 from statistics import mean
 
+def best_fit_slope(xs,ys):
+    slope=(mean(xs)*mean(ys))
+    slope=slope-(mean(xs*ys))
+    slope=slope/((mean(xs)**2)-(mean(xs**2)))
+    return slope
+
 xaxis=[1,2,3,4,5]
 yaxis=[1,4,9,16,25]
+xaxis=np.array(xaxis, dtype=np.float)
+yaxis=np.array(yaxis, dtype=np.float)
 
-plt.scatter(xaxis,yaxis)
-plt.show()
+slope=best_fit_slope(xaxis,yaxis)
+
+print(slope)
+# plt.scatter(xaxis,yaxis)
+# plt.show()
