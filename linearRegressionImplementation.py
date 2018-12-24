6824f86d694499030546d506ff69336ef814e5b3
eshwarne <northeasteditsofficial@gmail.com>
2018-12-24 16:08:50 +0530

    yIntercept

--- linearRegressionImplementation.py
+++ linearRegressionImplementation.py
@@ -7,14 +7,17 @@ def best_fit_slope(xs,ys):
     slope=slope-(mean(xs*ys))
     slope=slope/((mean(xs)**2)-(mean(xs**2)))
     return slope
-
-xaxis=[1,2,3,4,5]
-yaxis=[1,4,9,16,25]
-xaxis=np.array(xaxis, dtype=np.float)
-yaxis=np.array(yaxis, dtype=np.float)
+def get_yIntercept(ys,slope,xs):
+    yIntercept=(mean(ys))-(slope*(mean(xs)))
+    return yIntercept
+xaxis = [1,2,3,4,5]
+yaxis = [1,4,9,16,25]
+xaxis = np.array(xaxis, dtype=np.float)
+yaxis = np.array(yaxis, dtype=np.float)
 
 slope=best_fit_slope(xaxis,yaxis)
+yIntercept=get_yIntercept(yaxis,slope,xaxis)
+print(slope, " ",yIntercept)
 
-print(slope)
 # plt.scatter(xaxis,yaxis)
 # plt.show()
